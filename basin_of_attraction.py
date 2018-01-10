d1e15bf77f949442d0055947bf865d4249375146
ChristophorusX <mrxrtxrt@gmail.com>
2018-01-10 18:43:28 -0500

    update distance measure

--- basin_of_attraction.py
+++ basin_of_attraction.py
@@ -10,15 +10,17 @@ def landscape(A, z, max_radius):
     """
 
     info = []
+    dim, _ = A.shape
     radius_range = np.linspace(
         0, max_radius, num=5000, endpoint=True, retstep=False)
     for radius in radius_range:
         rand_ground = get_ground_truth(z)
-        pt = get_nearby_pt(rand_ground, radius)
+        pt, distance = get_nearby_pt(rand_ground, radius)
         func_value = func_val(A, pt)
         pred_value = pred_val(A, rand_ground, pt)
         diff = func_value - pred_value
-        info.append([diff, radius])
+        correlation = np.linalg.norm(z.ravel().dot(pt)) / dim
+        info.append([diff, distance, correlation])
     return np.array(info)
 
 
@@ -41,18 +43,20 @@ def draw_landscape(info):
     """Draw the difference against radius."""
 
     diff_arr = info[:, 0]
-    radius_arr = info[:, 1]
+    distance_arr = info[:, 1]
+    corr_arr = info[:, 2]
     plt.style.use('ggplot')
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
-    plt.scatter(radius_arr, diff_arr, alpha=0.5,
-                label=r'Point $Q\in\mathcal{M}$ near ground truth')
+    plt.scatter(distance_arr, diff_arr, alpha=0.5,
+                label=r'Point $Q\in\mathcal{M}$ near ground truth (color indicates correlation)',
+                c=corr_arr, cmap=plt.cm.get_cmap('Blues'), vmin=.5, vmax=1)
     plt.title(
         r'Local landscape near ground truth\\ compared to ``quadratic" approximation')
-    plt.xlabel(r'Radius $r$ to ground truth')
+    plt.xlabel(r'Distance $\|zz^T-QQ^T\|_F$ to ground truth')
     plt.ylabel(r'Difference $\mathrm{Tr}(AQ) -\mathrm{model}(Q)$')
-    plt.text(
-        0, -8, r'$\mathrm{model}(Q)=f(\bar Q(z))+\langle\mathrm{grad}f(\bar Q(z)),Q-\bar Q(z)\rangle-\frac{n}{2}\|Q-\bar Q(z)\|^2$')
+    # plt.text(
+    #     0, -8, r'$\mathrm{model}(Q)=f(\bar Q(z))+\langle\mathrm{grad}f(\bar Q(z)),Q-\bar Q(z)\rangle-\frac{n}{2}\|Q-\bar Q(z)\|^2$')
     plt.legend()
     plt.savefig('local landscape.png', dpi=250)
     plt.close('all')
@@ -139,10 +143,11 @@ def get_nearby_pt(ground_truth, distance):
     # project back onto the manifold
     for i in range(dim):
         point[i, :] = point[i, :] / np.linalg.norm(point[i, :])
-    return point
+    distance = np.linalg.norm(ground_truth.dot(ground_truth.T) - point.dot(point.T))
+    return point, distance
 
 
 if __name__ == '__main__':
-    A, z = get_observation(10, 10, 'positive-sparse')
-    info = landscape(A, z, max_radius=.5)
+    A, z = get_observation(10, 10, 'positive-rows')
+    info = landscape(A, z, max_radius=5)
     draw_landscape(info)
