89608185a024e01c333ae7125e074db3b7902005
ChristophorusX <mrxrtxrt@gmail.com>
2018-07-25 15:46:21 -0400

    add files

--- spurious_local_optima.m
+++ spurious_local_optima.m
@@ -1,7 +1,7 @@
 clc
 close all
 
-n = 20;
+n = 50;
 theta = [1:n]'/n*2*pi;
 %theta = 1.2*rand(n,1)*pi;
 %theta(1) = 0;
@@ -11,14 +11,17 @@ theta = [1:n]'/n*2*pi;
 x = cos(theta);
 y = sin(theta);
 Q = [x y];
+I = eye(n)
 
 cvx_begin
-cvx_precision high
+cvx_precision low
 variable A(n, n) symmetric
-minimize - lambda_min(diag(diag(A * Q * Q')) - A.*(Q*Q'))
+minimize -lambda_min(diag(diag(A * Q * Q')) - A.*(Q*Q'))
 subject to
-A(:) >= 1
-%            A(:) <= n
+diag(A) == 0
+A(:) + I(:) >= 1
+% A(:) >= 1
+A(:) <= sqrt(n)
 diag(x) * (A * y) == diag(y) * (A * x)
 cvx_end
 
